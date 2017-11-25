c0f622297d1a33a2032ef269d92d5aa5ac058111
Shea Lignitz <rlignitz@gmail.com>
2017-11-25 04:02:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+  }
+  return names;
+}
+
+function isItTails() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  let tailsCount = 0;
+  while(isItTails()) {
+    tailsCount++;
+  }
+  return `You got ${tailsCount} tails in a row!`;
+}
